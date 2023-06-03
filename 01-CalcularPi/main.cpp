74ee0c8366cab7d26253b1e2f6c7a0cb049c12d5
ronniemezzapesa <rmezzapesa@frba.utn.edu.ar>
2023-06-03 01:15:06 -0300

    rehecho

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 double hallarPi (){
     double suma=0.0;
     double den=1.0;
     int exp=4.0;
-    for ( int i=0 ; i < 1000000 ; i++){
+    while (fabs(suma-3.141592) >= 0.0000001){
         suma+= exp / den;
         den+=2;
         exp*=-1;
@@ -17,6 +18,5 @@ int main() {
  pi=hallarPi();
  cout << fixed << setprecision(6);
  cout << "El valor de Pi con sus 6 decimales es " << pi << endl;
-
-return 0;
+ return 0;
 }
