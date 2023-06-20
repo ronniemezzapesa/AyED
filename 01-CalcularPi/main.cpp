a12c01cfad5784f8dd7bba814230198fce428cc6
Ronnie Mezzapesa <134455759+ronniemezzapesa@users.noreply.github.com>
2023-06-20 18:56:07 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,20 +3,20 @@
 #include <cmath>
 using namespace std;
 double hallarPi (){
-    double suma=0.0;
+    double suma=0.0, ant=0.0;
     double den=1.0;
-    int exp=4.0;
-    while (fabs(suma-3.141592) >= 0.0000001){
+    int exp=1.0;
+    do {
+        ant=suma;
         suma+= exp / den;
         den+=2;
         exp*=-1;
-    }
-    return suma;
+    } while (fabsf(suma-ant) > 0.0000001);
+    return suma*4;
 }
 int main() {
- double pi=0;
- pi=hallarPi();
- cout << fixed << setprecision(6);
- cout << "El valor de Pi con sus 6 decimales es " << pi << endl;
- return 0;
+ double pi=hallarPi();
+ cout << fixed  << setprecision(6);
+ cout << "El valor de Pi con sus 6 decimales es " << trunc(pi*1000000)/1000000 << endl;
+return 0;
 }
